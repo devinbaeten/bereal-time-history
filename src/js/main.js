74c3d0102024c2d91160f85faceaa4a4bf1a63d0
Devin Baeten <devinbaeten@devin-mbp13.local>
2023-04-10 16:07:44 +0000

    Added Sentry

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,15 @@ import '../scss/styles.scss';
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 
+// Sentry
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://f7b9cd23b08549d496928e95863a508f@o4504770759229440.ingest.sentry.io/4504990691491840",
+  integrations: [new Sentry.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 // Import Bootstrap Icons
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
