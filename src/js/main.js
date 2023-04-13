312fe36e0b0440af4665487c9728fdb9af46f45e
Devin Baeten <devinbaeten@devin-mbp13.local>
2023-04-13 16:53:47 +0000

    Updated Sentry config

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,10 @@ import * as bootstrap from 'bootstrap';
 import * as Sentry from "@sentry/browser";
 
 Sentry.init({
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
   dsn: "https://f7b9cd23b08549d496928e95863a508f@o4504770759229440.ingest.sentry.io/4504990691491840",
-  integrations: [new Sentry.Replay(), new Sentry.BrowserTracing()],
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [new Sentry.Replay({maskAllText: false, maskAllInputs: false, blockAllMedia: false}), new Sentry.BrowserTracing()],
   tracesSampleRate: 1.0,
 });
 
